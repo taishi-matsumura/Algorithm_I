85b2fe0c95551345be13aa99f63bd4ab15521510
taishi-matsumura <taishi0706@gmail.com>
2020-09-03 03:15:38 +0900

    三好の解答を追加

--- Q2/BubbleSort.cpp
+++ Q2/BubbleSort.cpp
@@ -1,40 +1,31 @@
 ﻿#include <stdio.h>
 
 // プロトタイプ宣言
-void BubbleSort(int data[], int size);
-void Swap(int data[], int right, int left);
 void PrintArray(int data[], int size);
 
 
 // メイン関数
 int main() {
-	int array[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-	int arraySize = sizeof(array) / sizeof(int);
+	int data[] = { 5, 4, 3, 2, 1 };
 
-	printf("ソート前の状態\n");
-	PrintArray(array, arraySize);
+	printf("ソート前の状態：");
+	PrintArray(data, 5);
 
-	BubbleSort(array, arraySize);
-}
+	printf("\nソート開始\n");
 
-/// <summary>
-/// バブルソート処理の本体
-/// </summary>
-/// <param name="data">ソート対象の配列</param>
-/// <param name="size">配列の要素数</param>
-void BubbleSort(int data[], int size) {
-}
+	for (int i = 0; i <= 4; i++) {
+		int temp = data[i];
+		if (data[i - 1] > temp) {
+			int j = i;
+			do {
+				data[j] = data[j - 1];
+				j = j - 1;
+			} while (j > 0 && data[j - 1] > temp);
+			data[j] = temp;
+		}
 
-/// <summary>
-/// 配列内の2つの要素を入れ替える
-/// </summary>
-/// <param name="data">入れ替える対象を含む配列</param>
-/// <param name="index1">入れ替え対象の要素番号</param>
-/// <param name="index2">入れ替え対象の要素番号</param>
-void Swap(int data[], int index1, int index2) {
-	int tmp = data[index1];
-	data[index1] = data[index2];
-	data[index2] = tmp;
+		PrintArray(data, 5);
+	}
 }
 
 /// <summary>
