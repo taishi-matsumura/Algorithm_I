f3f8e9c147a4859b65810ca977a270fd74ec55bf
taishi-matsumura <taishi0706@gmail.com>
2020-09-03 02:17:25 +0900

    参考プログラムを作成
    - 問1のフローチャートに相当するC言語プログラムを作成。
    - 問2のプログラムの大枠を作成。バブルソートの処理は空の状態。

---
+++ Q1/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+// 問1 (2)
+// これは「挿入ソート」
+void Sort(int data[], int arraySize) {
+	for (int i = 1; i < arraySize; i++) {
+		int tmp = data[i];
+
+		if (data[i - 1] > tmp) {
+			int j = i;
+
+			do {
+				data[j] = data[j - 1];
+				j--;
+
+				// 問1 (1)
+				// この時点でのi、j、data[]の値を表示する
+				printf("i = %d, j = %d, data = {", i, j);
+				for (int index = 0; index < arraySize; index++) {
+					printf(" %d", data[index]);
+				}
+				printf("}\n");
+
+			} while (j > 0 && data[j - 1] > tmp);
+
+			data[j] = tmp;
+		}
+	}
+}
+
+int main() {
+	int array[] = { 3,1,5,4,2 };
+
+	Sort(array, sizeof(array) / sizeof(int));
+}
---
+++ Q2/BubbleSort.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+
+// プロトタイプ宣言
+void BubbleSort(int data[], int size);
+void Swap(int data[], int right, int left);
+void PrintArray(int data[], int size);
+
+
+// メイン関数
+int main() {
+	int array[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	int arraySize = sizeof(array) / sizeof(int);
+
+	printf("ソート前の状態\n");
+	PrintArray(array, arraySize);
+
+	BubbleSort(array, arraySize);
+}
+
+/// <summary>
+/// バブルソート処理の本体
+/// </summary>
+/// <param name="data">ソート対象の配列</param>
+/// <param name="size">配列の要素数</param>
+void BubbleSort(int data[], int size) {
+}
+
+/// <summary>
+/// 配列内の2つの要素を入れ替える
+/// </summary>
+/// <param name="data">入れ替える対象を含む配列</param>
+/// <param name="index1">入れ替え対象の要素番号</param>
+/// <param name="index2">入れ替え対象の要素番号</param>
+void Swap(int data[], int index1, int index2) {
+	int tmp = data[index1];
+	data[index1] = data[index2];
+	data[index2] = tmp;
+}
+
+/// <summary>
+/// 配列の内容を画面に表示する
+/// </summary>
+/// <param name="data">表示する配列</param>
+/// <param name="size">配列の要素数</param>
+void PrintArray(int data[], int size) {
+	printf("%d", data[0]);
+
+	for (int i = 1; i < size; i++) {
+		printf(", %d", data[i]);
+	}
+
+	printf("\n");
+}
