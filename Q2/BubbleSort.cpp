9266f0564636aa4b541b143b6c07a099b24bbe4c
TastuyaTsujimoto <machikado111@gmail.com>
2020-09-07 04:09:30 +0900

    BubbleSort関数、Swap関数を生成 関数の説明追加 While文をfor文に修正

--- Q2/BubbleSort.cpp
+++ Q2/BubbleSort.cpp
@@ -2,31 +2,19 @@
 
 // プロトタイプ宣言
 void PrintArray(int data[], int size);
-
+void BubbleSort(int data[], int ArraySize);
+void Swap(int* Swap_target, int* Swap_base);
 
 // メイン関数
 int main() {
-	int data[] = { 5, 4, 3, 2, 1 };
+	int data[] = { 5, 3, 1, 8, 2, 4, 6 };
+	int ArraySize = sizeof(data) / sizeof(int);
 
 	printf("ソート前の状態\n");
-	PrintArray(data, 5);
+	PrintArray(data, ArraySize);
+	printf("ソート後の状態\n");
+	BubbleSort(data, ArraySize);
 
-	int tmp;
-	int j;
-	for (int i = 0; i <= 4; i++) {
-		j = 4;
-		while (j > i) {
-			tmp = data[j];
-			if (data[j - 1] > tmp) {
-				data[j] = data[j - 1];
-				data[j - 1] = tmp;
-			}
-
-			j--;
-		
-			PrintArray(data, 5);
-		}
-	}
 }
 
 /// <summary>
@@ -43,3 +31,24 @@ void PrintArray(int data[], int size) {
 
 	printf("\n");
 }
+
+void BubbleSort(int data[], int ArraySize)
+{
+	int Element_number_MAX = ArraySize - 1;
+	for (int i = 0; i < ArraySize; i++) {
+		for (int j = Element_number_MAX; j > i; j--) {
+			if (data[j] < data[j - 1]) {
+				Swap(&data[j - 1], &data[j]);
+			}
+			PrintArray(data, ArraySize);
+		}
+	}
+}
+
+void Swap(int* Swap_target, int* Swap_base)
+{
+	int tmp = *Swap_base;
+	*Swap_base = *Swap_target;
+	*Swap_target = tmp;
+}
+
