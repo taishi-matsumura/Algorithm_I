c42d769c8e18e36cdb45a35e359bda3c21de6c05
TastuyaTsujimoto <machikado111@gmail.com>
2020-09-09 15:43:01 +0900

    変数名修正

--- Q2/BubbleSort.cpp
+++ Q2/BubbleSort.cpp
@@ -1,16 +1,16 @@
-﻿#include <stdio.h>
+﻿#include<stdio.h>
 
 // プロトタイプ宣言
 void PrintArray(int data[], int size);
 void BubbleSort(int data[], int ArraySize);
-void Swap(int* Swap_target, int* Swap_base);
+void Swap(int* SwapTarget, int* SwapBase);
 
 
-const int Element_number_MIN = 0;		//要素番号の最初は確定で０、かつ変動しないのでグローバル変数に
+const int ElementNumberMIN = 0;		//要素番号の最初は確定で０、かつ変動しないのでグローバル変数に
 
 // メイン関数
 int main() {
-	int data[] = { 5, 3, 1, 8, 2, 4, 6 };
+	int data[] = { 5, 3, 1, 8, 2, 9, 4, 6, 7 };
 	int ArraySize = sizeof(data) / sizeof(int);		//要素数＝要素/要素１つ当たりの長さ（のイメージ）
 
 	printf("ソート前の状態\n");
@@ -26,7 +26,7 @@ int main() {
 /// <param name="data">表示する配列</param>
 /// <param name="size">配列の要素数</param>
 void PrintArray(int data[], int ArraySize) {
-	printf("%d", data[Element_number_MIN]);
+	printf("%d", data[ElementNumberMIN]);
 	for (int i = 1; i < ArraySize; i++) {
 		printf(", %d", data[i]);
 	}
@@ -41,9 +41,9 @@ void PrintArray(int data[], int ArraySize) {
 /// <param name="ArraySize">配列の要素数</param>
 void BubbleSort(int data[], int ArraySize)
 {
-	int Element_number_MAX = ArraySize - 1;
-	for (int i = Element_number_MIN; i < ArraySize; i++) {
-		for (int j = Element_number_MAX; j > i; j--) {		//j,j-1を置き換えることも考えたけれど
+	int ElementNumberMAX = ArraySize - 1;
+	for (int i = ElementNumberMIN; i < ArraySize; i++) {
+		for (int j = ElementNumberMAX; j > i; j--) {		//j,j-1を置き換えることも考えたけれど
 			if (data[j] < data[j - 1]) {					//弄ることもない上に見た目もわかりやすいのでこのままに
 				Swap(&data[j - 1], &data[j]);
 			}
@@ -56,10 +56,10 @@ void BubbleSort(int data[], int ArraySize)
 /// </summary>
 /// <param name="Swap_target">入れ替え対象の要素</param>
 /// <param name="Swap_base">入れ替えの基となる要素</param>
-void Swap(int* Swap_target, int* Swap_base)
+void Swap(int* SwapTarget, int* SwapBase)
 {
-	int tmp = *Swap_base;
-	*Swap_base = *Swap_target;
-	*Swap_target = tmp;
+	int tmp = *SwapBase;
+	*SwapBase = *SwapTarget;
+	*SwapTarget = tmp;
 }
 
