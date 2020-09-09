be86af7f8230870f3b9b80359ab8238276c1732d
Miyoshi-Takuto <miyoshi.taku10@gmail.com>
2020-09-10 00:23:58 +0900

    ソートの天秤を右から左へ移動させるように変更 ソートを関数化

--- Q2/BubbleSort.cpp
+++ Q2/BubbleSort.cpp
@@ -2,26 +2,33 @@
 
 // プロトタイプ宣言
 void PrintArray(int data[], int size);
+void SortArray(int data[], int size);
 
 
 // メイン関数
 int main() {
-	int data[] = { 9,2,4,1,3 };
+	int data[] = { 3,4,1,5,2 };
 
 	printf("ソート前の状態：");
 	PrintArray(data, 5);
 
 	printf("\nソート開始\n");
 
-	for (int i = 0; i <= 4; i++) {
-		for (int j = i + 1; j <= 4; j++) {
+	SortArray(data, 5);
+
+}
+
+void SortArray(int data[], int size) {
+
+	for (int i = size - 1; i >= 0; i--) {
+		for (int j = i - 1; j >= 0; j--) {
 			int temp = data[i];
-			if (temp > data[j]) {
+			if (temp < data[j]) {
 				data[i] = data[j];
 				data[j] = temp;
 			}
 		}
-		PrintArray(data, 5);
+		PrintArray(data, size);
 	}
 }
 
